2d6cc018c7b88df1b97122b4eb874ea326d16393
rof1yev <rofiyevdilshod@gmail.com>
2023-12-25 17:54:00 +0500

    #2 v-for, attribute binding and dynamic classes

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       name: "Dilshod",
+      url: "https://rof1yev-blog.vercel.app/",
+      avatar: "./assets/author.jpg",
+      programmingLanguages: [
+        { name: "JavaScript", rating: 9, isFavorite: true },
+        { name: "Python", rating: 10, isFavorite: true },
+        { name: "Java", rating: 9, isFavorite: false },
+        { name: "C#", rating: 7, isFavorite: false },
+      ],
     };
   },
   methods: {
-    handleEvent(e, number) {
-      console.log(number, e.type);
+    toggleFavorite(language) {
+      language.isFavorite = !language.isFavorite;
     },
   },
 });
